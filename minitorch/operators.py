81c6a12d8f4c2fa969182f4928dcc1fc5e2c91a1
Khong Ngoc Anh <khongngocanh2004@gmail.com>
2024-09-07 09:16:57 +0700

    first commit

--- minitorch/operators.py
+++ minitorch/operators.py
@@ -31,6 +31,56 @@ from typing import Callable, Iterable
 # For is_close:
 # $f(x) = |x - y| < 1e-2$
 
+def mul(x: float, y: float) -> float:
+    return x * y
+
+def id(x: float) -> float:
+    return x
+
+def add(x: float, y: float) -> float:
+    return x + y
+
+def neg(x: float) -> float:
+    return -x
+
+def lt(x: float, y: float) -> bool:
+    return x < y
+
+def eq(x: float, y: float) -> bool:
+    return x == y
+
+def max(x: float, y: float) -> float:
+    return max(x, y)
+
+def is_close(x: float, y: float) -> bool:
+    return abs(x - y) < 1e-2
+
+def sigmoid(x: float) -> float:
+    if x >= 0:
+        return 1.0 / (1.0 + math.exp(-x))
+    else:
+        return math.exp(x) / (1.0 + math.exp(x))
+    
+def relu(x: float) -> float:
+    return max(0, x)
+
+def log(x: float) -> float:
+    return math.log(x)
+
+def exp(x: float) -> float:
+    return math.exp(x)
+
+def inv(x: float) -> float:    
+    return 1 / x
+
+def log_back(x: float) -> float:
+    return 1 / x    
+
+def inv_back(x: float) -> float:
+    return -1 / (x ** 2)
+
+def relu_back(x: float) -> float:
+    return 1 if x > 0 else 0
 
 # TODO: Implement for Task 0.1.
 
